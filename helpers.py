95a8c637661f5955de58eb55a7035503137332c2
alimohamed39 <ali.m.eid61@gmail.com>
2024-06-19 14:22:24 +0200

    add augmentation logic and enable/disable by setting the variable

--- helpers.py
+++ helpers.py
@@ -7,6 +7,9 @@ from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 import joblib
+import os
+import librosa
+import soundfile as sf
 
 
 def plot_feature(feat, feat_name, snip_meta):
@@ -156,7 +159,7 @@ def convert_int_to_label(label_index):
 ## FEATURE IMPORTANCE SECTION DO NOT TOUCH!!!!!!!!
 
 #load model
-rfc = joblib.load('random_forest_model.pkl')
+rfc = joblib.load('random_forest_model_best.pkl')
 feature_importances = rfc.feature_importances_
 
 #load csv file with feature names and their index
@@ -195,3 +198,120 @@ def get_most_important_features_names(n):
 
 def get_feature_index_from_name(name):
     return feature_names.index(name)
+
+
+def add_white_noise(wave, noise_factor: float):
+   """
+   :param wave: our scenes
+   :param noise_factor: how strong the noise shouldbe
+   :return:a white noise augmented image
+   """
+   noise = np.random.normal(0, wave.std(), wave.shape)
+   augmented_wave = wave + noise * noise_factor
+   return augmented_wave
+
+
+def add_time_stretch(wave,rate:float):
+    """
+    Makes the recording slower or faster. I think it will be more beneficial in our task if the recordings
+    are bit faster
+    :param wave: your npy file wave
+    :param rate: the speed in which you want to stretch the audio
+    :return: augmented npy files
+    """
+
+    return librosa.effects.time_stretch(wave,rate=rate)
+
+
+
+def pitch_shifting(wave,sr,n_steps):
+
+    return librosa.effects.pitch_shift(wave,sr=sr,n_steps=n_steps)
+
+
+def apply_augmentation_folder(inp_dir,out_dir,noise_factor:float,n_steps,rate):
+    """
+    Gets the data and loads it ,then apply three different augmentations
+    with an option of plotting them
+    :param inp_dir:
+    :param out_dir:
+    :param noise_factor: controlling factor of the noise
+    :param n_steps: n_steps for the stretch rate
+    :param rate: rate of the pitching scale
+    :return: augmented npy files in the out_dir
+    """
+    #make the out_dir if it doesnt exist
+    os.makedirs(out_dir,exist_ok=True)
+
+    files = os.listdir(inp_dir)
+
+    n_of_files = 0
+    n_noise = 0
+    n_time_stretch = 0
+    n_pitch_scaled = 0
+    augmented_data = []
+
+    def plot_signal(signal, augmented_signal):
+        fig, ax = plt.subplots(nrows=2)
+        librosa.display.waveshow(signal, sr=sr, ax=ax[0])
+        ax[0].set(title=f"Original{file}")
+        librosa.display.waveshow(augmented_signal, sr=sr, ax=ax[1])
+        ax[1].set(title="Augmented")
+        plt.show()
+
+    for file in files :
+        if file.endswith(".npy"):
+
+           file_path = os.path.join(inp_dir, file)
+           wave = np.load(file_path)
+           sr = 160000
+           try:
+               if n_of_files <= 300:
+                   augmented_wave = add_white_noise(wave=wave, noise_factor=noise_factor)
+                   augmentation_name = "noise"
+                   n_noise += 1
+
+
+
+               elif 300 < n_of_files <= 550:
+                   augmented_wave = add_time_stretch(wave, rate=5)
+                   augmentation_name = "time_stretched"
+                   # n_of_files += 1
+                   n_time_stretch += 1
+
+
+
+
+
+               else:
+                   augmented_wave = pitch_shifting(wave=wave, sr=sr, n_steps=0.2)
+                   augmentation_name = "pitch_scaled"
+                   n_pitch_scaled += 1
+
+                   augmented_wave = add_white_noise(wave,0.5)
+
+                   clean_file_name = file.replace(" ", "_").replace(",", "_").replace(";", "_")
+                   out_file = os.path.join(out_dir, "augmented_" + clean_file_name)
+
+                   np.save(out_file, augmented_wave)
+                   print("File succesfully augmented")
+
+                   if np.array_equal(wave, augmented_wave):
+                        print("Warning: Augmented npy file is identical to original,check if augmentation worked!")
+
+                   augmented_data.append(augmented_wave)
+           except:
+               print("An error occured!")
+
+
+
+
+           return np.array(augmented_data)
+
+
+
+
+        print(n_of_files)
+        print(f"num of noise augmented:{n_noise}")
+        print(f"num of pitch scaled augmented:{n_pitch_scaled}")
+        print(f"num of time stretched augmented:{n_time_stretch}")
--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import csv
 import joblib
-=========
->>>>>>>>> Temporary merge branch 2
-import joblib
 import numpy as np
 import sklearn
 from matplotlib import pyplot as plt
@@ -13,8 +10,9 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 import os
 
+
 from helpers import plot_spectrogram, calc_deltas, flatten_data_by_mean, normalize_data, get_data_for_speakers, \
-    get_label_map, filter_by_label,add_white_noise,add_time_stretch,pitch_shifting
+    get_label_map, filter_by_label
 
 import helpers
 
@@ -87,6 +85,132 @@ def setup_knn(data):
 
 
 #recording_folds = setup_knn(data)
+
+def add_white_noise(wave, noise_factor: float):
+   """
+   :param wave: our scenes
+   :param noise_factor: how strong the noise shouldbe
+   :return:a white noise augmented image
+   """
+   noise = np.random.normal(0, wave.std(), wave.shape)
+   augmented_wave = wave + noise * noise_factor
+   return augmented_wave
+
+
+def add_time_stretch(wave,rate:float):
+    """
+    Makes the recording slower or faster. I think it will be more beneficial in our task if the recordings
+    are bit faster
+    :param wave: your npy file wave
+    :param rate: the speed in which you want to stretch the audio
+    :return: augmented npy files
+    """
+
+    return librosa.effects.time_stretch(wave,rate=rate)
+
+
+
+def pitch_shifting(wave,sr,n_steps):
+
+    return librosa.effects.pitch_shift(wave,sr=sr,n_steps=n_steps)
+
+def apply_augmentation_nparray(data,  noise_factor: float, n_steps, rate):
+    data
+
+def apply_augmentation_folder(inp_dir,out_dir,noise_factor:float,n_steps,rate):
+    """
+    Gets the data and loads it ,then apply three different augmentations
+    with an option of plotting them
+    :param inp_dir:
+    :param out_dir:
+    :param noise_factor: controlling factor of the noise
+    :param n_steps: n_steps for the stretch rate
+    :param rate: rate of the pitching scale
+    :return: augmented npy files in the out_dir
+    """
+    #make the out_dir if it doesnt exist
+    os.makedirs(out_dir,exist_ok=True)
+
+    files = os.listdir(inp_dir)
+
+    n_of_files = 0
+    n_noise = 0
+    n_time_stretch = 0
+    n_pitch_scaled = 0
+    augmented_data = []
+
+    def plot_signal(signal, augmented_signal):
+        fig, ax = plt.subplots(nrows=2)
+        librosa.display.waveshow(signal, sr=sr, ax=ax[0])
+        ax[0].set(title=f"Original{file}")
+        librosa.display.waveshow(augmented_signal, sr=sr, ax=ax[1])
+        ax[1].set(title="Augmented")
+        plt.show()
+
+    for file in files :
+        if file.endswith(".npy"):
+
+           file_path = os.path.join(inp_dir, file)
+           wave = np.load(file_path)
+           sr = 160000
+           try:
+               if n_of_files <= 300:
+                   augmented_wave = add_white_noise(wave=wave, noise_factor=noise_factor)
+                   augmentation_name = "noise"
+                   n_noise += 1
+
+
+
+               elif 300 < n_of_files <= 550:
+                   augmented_wave = add_time_stretch(wave, rate=5)
+                   augmentation_name = "time_stretched"
+                   # n_of_files += 1
+                   n_time_stretch += 1
+
+
+
+
+
+               else:
+                   augmented_wave = pitch_shifting(wave=wave, sr=sr, n_steps=0.2)
+                   augmentation_name = "pitch_scaled"
+                   n_pitch_scaled += 1
+
+                   augmented_wave = add_white_noise(wave,0.5)
+
+                   clean_file_name = file.replace(" ", "_").replace(",", "_").replace(";", "_")
+                   out_file = os.path.join(out_dir, "augmented_" + clean_file_name)
+
+                   np.save(out_file, augmented_wave)
+                   print("File succesfully augmented")
+
+                   if np.array_equal(wave, augmented_wave):
+                        print("Warning: Augmented npy file is identical to original,check if augmentation worked!")
+
+                   augmented_data.append(augmented_wave)
+           except:
+               print("An error occured!")
+
+
+
+
+           return np.array(augmented_data)
+
+
+
+
+        print(n_of_files)
+        print(f"num of noise augmented:{n_noise}")
+        print(f"num of pitch scaled augmented:{n_pitch_scaled}")
+        print(f"num of time stretched augmented:{n_time_stretch}")
+
+
+
+
+
+
+
+
 
 """
 Set up SVM
